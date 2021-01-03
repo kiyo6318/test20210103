4271da411602d116b82fb270fa74e7e3f65682af
kiyo <kiyonari6318@yahoo.co.jp>
2021-01-03 15:28:59 +0900

    Fix: レイアウトを修正

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,15 @@
 import React, {ReactNode} from 'react'
 import Link from 'next/link'
-import Logo from '../public/images/logo.svg';
 import styles from '../styles/Header.module.css'
-import Facebook from '../public/images/facebook-icon.svg'
-import Instagram from '../public/images/instagram-icon.svg'
-import Email from '../public/images/email-icon.svg'
 
 export default function Header() {
   return (
     <div className={styles.container}>
       <div className={styles.flex_container}>
-        {/* <Logo className={styles.logo}/> */}
-        {/* <div className={styles.logo}><img src="/images/logo.svg" /></div> */}
-        <img className={styles.logo} src="/images/logo.svg" />
+        <img className={styles.logo} src="/images/nogi-store-logo.svg" />
         <div className={styles.external}>
-          {/* <Instagram className={styles.instagram} /> */}
-          {/* <Facebook className={styles.facebook} /> */}
-          {/* <Email className={styles.email} /> */}
           <img className={styles.instagram} src="/images/instagram-icon.svg" />
           <img className={styles.facebook} src="/images/facebook-icon.svg" />
-          {/* <img className={styles.facebook} src="/images/facebook-square.svg" /> */}
           <img className={styles.email} src="/images/email-icon.svg" />
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>nogi store</title>
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Items/>
     </div>
