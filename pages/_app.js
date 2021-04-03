69b7e47ba6d1ae5af16c47c59078b7371b552ec1
kiyo <kiyonari6318@yahoo.co.jp>
2021-04-03 23:38:42 +0900

    Fix: title修正

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import '../styles/globals.css';
 import { Container } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="all_wrapper">
+      <Head>
+        <title>nogi store</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Items from '../components/Items';
-import { useMediaQuery } from 'react-responsive';
 import React from 'react';
 
 export default function Home() {
   return (
     // <div className={isPcScreen ? styles.pc_container : styles.sp_container}>
     <>
-      <Head>
-        <title>nogi store</title>
-      </Head>
       <Items />
     </>
   );
