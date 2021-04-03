80ba5ea502cc0d5fa753cf528d91f2a2bdd00c22
kiyo <kiyonari6318@yahoo.co.jp>
2021-04-03 20:51:09 +0900

    Fix: image null判定追加

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -8,9 +8,11 @@ const ProjectId = ({ project }) => {
   return (
     <div className={styles.pc_container}>
       <div className={styles.title}>{project.title_ja}</div>
-      <div className={styles.img}>
-        <img src={project.image.url} alt="image" />
-      </div>
+      {project.image ? (
+        <div className={styles.img}>
+          <img src={project.image.url} alt="image" />
+        </div>
+      ) : null}
       <p className={styles.category_ja}>{project.category_ja}</p>
       <p className={styles.year}>{project.year}</p>
       <div className={styles.body} dangerouslySetInnerHTML={createMarkup()} />
